2ff0f151f7cd2daf0c8e0fb4459ab1367c20ca40
m bobak <mbobak@illinois.edu>
2023-02-07 15:43:21 -0600

    easier to change default bucket, but still needs testing

--- summarize/src/get_repo.py
+++ summarize/src/get_repo.py
@@ -2,17 +2,19 @@
 #M Bobak, need to get gleaner output for a repo
 # for now have to pull all files to turn to quads here ;till issue126 
 import os
+#import ec
+import earthcube_utilities as ec  #make sure this is availabe
 #for now keeping in a dir named after each repo, so next step can run2nq over it
  #where it takes the it's filename, to turn the triples into quads w/that name
 
-def get_repo(repo):
-    #import ec
-    import earthcube_utilities as ec
+#def get_repo(repo):
+def get_repo(repo, default_bucket= "https://oss.geocodes.ncsa.illinois.edu/"):
     #I think it uses cwd for repo, but want to override that
-    if not repo:
-        #print(f'cwd={ec.cwd} used to be the dir you would download the repo-s files into')
-        repo = ec.path_leaf(ec.cwd)
-        print(f'using: cwd={ec.cwd} to get the repo={repo} to downloads files into')
+    if not repo:  
+        #was cwd=ec.cwd, which are the same
+        cwd=os.getcwd()
+        repo = ec.path_leaf(cwd)
+        print(f'using: cwd={cwd} to get the repo={repo} to downloads files into')
     else:
         print(f'will get files for {repo} in a dir named after it')
         exists=os.path.exists(repo)
@@ -23,15 +25,27 @@ def get_repo(repo):
         cwd=os.getcwd()
         print(f'cd to: cwd={cwd} to get the repo={repo} to downloads files into')
     #ec.wget_oss_repo(repo) #defaults to bucket=ncsa_minio =https://oss.geocodes.ncsa.illinois.edu/
-    ec.wget_oss_repo(repo=None,path="gleaner/milled",bucket="https://oss.geocodes.ncsa.illinois.edu/")
+    ec.wget_oss_repo(repo=repo,path="gleaner/milled",bucket=default_bucket)
 
 
 if __name__ == '__main__':
     import sys
+    print(f'argv={sys.argv}')
+    default_bucket= "https://oss.geocodes.ncsa.illinois.edu/"
+    s3=os.getenv("S3ADDRESS") 
+    if ec.is_str(s3):
+        if not ec.is_http(s3):
+            s3="https://" + s3
+        print(f'set default_bucket to S3ADDRESS={s3}')
+        default_bucket=s3
+    if(len(sys.argv)>2):
+        default_bucket = sys.argv[1]
+        print(f'reset default_bucket to 2nd cli arg:{default_bucket}')
     if(len(sys.argv)>1):
         repo = sys.argv[1]
         print(f'will run over:{repo}')
-        get_repo(repo)
+        #get_repo(repo)
+        get_repo(repo,default_bucket)
         #run2nq(repo) #done in next step
     else:
         print("use cwd=repo")
