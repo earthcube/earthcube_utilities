c9121d0c21920cf9db6404d5c2255227f1eb069a
m bobak <mbobak@illinois.edu>
2023-02-08 12:53:15 -0600

    sh look down, import from ln-s up

--- summarize/summarize_repo.sh
+++ summarize/summarize_repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-../src/ec_summarize/fnq.py $1
+./src/fnq.py $1
 echo wait_for_fuseki_to_come_up ; sleep 20
-../../src/ec_summarize/tsum.py $1 |egrep -v "not IN_COLAB|rdf_inited|try:http"|cat>$1.ttl
+./src/tsum.py $1 |egrep -v "not IN_COLAB|rdf_inited|try:http"|cat>$1.ttl
