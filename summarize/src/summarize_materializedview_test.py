0b9c7fbd35a695d74e519ff18be3d96e4ccf57e2
David Valentine <david.valentine@gmail.com>
2023-02-26 11:23:59 -0800

    Basic Approval Test for summarize

--- summarize/src/summarize_materializedview_test.py
+++ summarize/src/summarize_materializedview_test.py
@@ -1,4 +1,5 @@
 import unittest
+from approvaltests.approvals import verify
 
 import pandas
 
@@ -6,6 +7,8 @@ import summarize.src.summarize_materializedview
 
 
 class SummarizeMaterializedViewTestCase(unittest.TestCase):
+
+
     def test_summaryDF2ttl(self):
         file = '../resources/testing/summarydf_short.csv'
         # with open(file, 'r') as f:
@@ -13,7 +16,12 @@ class SummarizeMaterializedViewTestCase(unittest.TestCase):
         testdf = pandas.read_csv(file)
         results = summarize.src.summarize_materializedview.summaryDF2ttl(testdf, "test")
         self.assertEqual(2, results.count("a :Dataset ;"))  # add assertion here
-
+        # this is an approval test.
+        # in the approved_files directory there will be a file .approved.txt
+        # if the test fails, but the results are correct,
+        # mv summarize/src/approved_files/SummarizeMaterializedViewTestCase.test_summaryDF2ttl.recieved.txt summarize/src/approved_files/SummarizeMaterializedViewTestCase.test_summaryDF2ttl.approved.txt
+        # rerun test
+        verify(results)
 
 if __name__ == '__main__':
     unittest.main()
