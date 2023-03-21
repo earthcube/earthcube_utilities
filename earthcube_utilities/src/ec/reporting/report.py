dcb662a0b68cefcb03f2adc2e9566d9df534b49e
David Valentine <david.valentine@gmail.com>
2023-03-21 11:24:23 -0700

    thoughts on reports

--- earthcube_utilities/src/ec/reporting/report.py
+++ earthcube_utilities/src/ec/reporting/report.py
@@ -24,9 +24,29 @@ class Repository():
     def GetReport(self,bucket, repo):
         pass
 
-def compareSummoned2Milled(bucket, repo):
-    """ return list of missing urls"""
+def compareSummoned2Milled(bucket, repo, datastore):
+    """ return list of missing urns/urls
+    Generating milled will be good to catch such errors"""
+    # compare using s3, listJsonld(bucket, repo) to  listMilledRdf(bucket, repo)
     pass
 
-def compareSummoned2Graph(bucket, repo):
+def compareSummoned2Graph(bucket, repo, datastore, graphendpoint):
+    """ return list of missing .
+    we do not alway generate a milled.
+    """
+    # compare using s3, listJsonld(bucket, repo) to queryWithSparql("repo_select_graphs", graphendpoint)
+    pass
+
+def graphTypes4Repo(repo, graphendpoint):
+    #queryWithSparql("repo_count_types", graphendpoint)
+    pass
+
+def graphTypes4RepoReport(repo, datastore):
+    pass
+
+def graphKeywordsRepo(repo, graphendpoint):
+    #queryWithSparql("repo_count_keywords", graphendpoint)
+    pass
+
+def graphKeywords4RepoReport(repo, datastore):
     pass
