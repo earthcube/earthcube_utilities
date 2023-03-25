65a7e27665385065e8c5f1ab93845d23d468056e
David Valentine <david.valentine@gmail.com>
2023-03-25 13:00:05 -0700

    sitemap

---
+++ earthcube_utilities/src/ec/check_sitemap.py
@@ -0,0 +1,30 @@
+from io import StringIO
+
+from ec.sitemap.sitemap import Sitemap
+import argparse
+
+def sitemap_checker(args):
+    sitemap = Sitemap(args.sitemapurl)
+    if  not args.nocheck :
+        sitemap.check_urls()
+
+    out = StringIO()
+    sitemap.sitemap_df.to_csv(out)
+    return out.getvalue()
+
+def start():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sitemapurl", help='sitemapurl')
+    parser.add_argument("--output", type=argparse.FileType('rw'), help='output file')
+    parser.add_argument("--no-url-check", dest="nocheck" ,help='output file', default=False)
+    args = parser.parse_args()
+
+    result = sitemap_checker(args)
+    if args.output:
+        args.output.write(result)
+    else:
+        print(result)
+if __name__ == '__main__':
+
+    result = start()
+
