6aa738afb3b505974b4dd27b5d3a5e037524c71c
David Valentine <david.valentine@gmail.com>
2023-07-05 17:39:11 -0700

    summary of relase graph

--- earthcube_utilities/src/ec/graph/release_graph.py
+++ earthcube_utilities/src/ec/graph/release_graph.py
@@ -5,9 +5,9 @@ from rdflib.namespace import RDF
 import ec.graph.sparql_query
 from ec.datastore.s3 import MinioDatastore
 
-summary_sparql = """
-# prefix schema: <https://schema.org/>
-SELECT distinct ?subj ?g ?resourceType ?name ?description  ?pubname
+"""
+prefix schema: <https://schema.org/>
+SELECT distinct ?g ?subj  ?resourceType ?name ?description  ?pubname
         (GROUP_CONCAT(DISTINCT ?placename; SEPARATOR=", ") AS ?placenames)
         (GROUP_CONCAT(DISTINCT ?kwu; SEPARATOR=", ") AS ?kw) ?datep ?sosType
 
@@ -15,13 +15,14 @@ SELECT distinct ?subj ?g ?resourceType ?name ?description  ?pubname
           graph ?g {
              ?subj schema:name ?name .
              ?subj schema:description ?description .
-            values ?sosType {
-            schema:Dataset
-
-            }
+                values ?sosType {
+                schema:Dataset
+    
+                }
 
             Minus {?subj a schema:Person } .
- BIND (IF (exists {?subj a schema:Dataset .} ||exists{?subj a schema:DataCatalog .} , "data", "tool") AS ?resourceType).
+            
+            #BIND (?type AS ?resourceType) .
 
             optional {?subj schema:distribution/schema:url|schema:subjectOf/schema:url ?url .}
             OPTIONAL {?subj schema:datePublished ?date_p .}
@@ -35,20 +36,67 @@ SELECT distinct ?subj ?g ?resourceType ?name ?description  ?pubname
              }
 
         }
-        GROUP BY ?subj ?pubname ?placenames ?kw ?datep   ?name ?description  ?resourceType ?sosType ?g
+      #  GROUP BY ?g ?subj ?resourceType ?name ?description ?pubname ?placenames ?kw ?datep    ?sosType 
+       GROUP BY ?g ?subj  ?name ?description ?pubname ?placenames ?kw ?datep    ?sosType 
+"""
+# notes... we can group concat where a field is possibly none. we have to use a bind.
+# from placenames and keywords
+summary_sparql = """
+prefix schema: <https://schema.org/>
+SELECT distinct ?g ?subj  ?resourceType ?name ?description  ?pubname
+        (GROUP_CONCAT(DISTINCT ?placename; SEPARATOR=", ") AS ?placenames)
+        (GROUP_CONCAT(DISTINCT ?kws; SEPARATOR=", ") AS ?kw) 
+        ?datep ?sosType
+
+        WHERE {
+          graph ?g {
+             ?subj schema:name ?name .
+             ?subj schema:description ?description .
+                values ?sosType {
+                schema:Dataset
+
+                }
+
+?subj a ?type . 
+            BIND (?type AS ?resourceType) .
+
+            optional {?subj schema:distribution/schema:url|schema:subjectOf/schema:url ?url .}
+            OPTIONAL {?subj schema:datePublished ?date_p .}
+            OPTIONAL {?subj schema:publisher/schema:name|schema:sdPublisher|schema:provider/schema:name ?pub_name .}
+            OPTIONAL {?subj schema:spatialCoverage/schema:name ?placename .}
+            OPTIONAL {?subj schema:keywords ?kwu } .
+
+            # BIND ( IF ( BOUND(?date_p), ?date_p, "1900-01-01") as ?datep ) .
+         #   BIND ( IF ( BOUND(?pub_name), ?pub_name, "No Publisher") as ?pubname ) .
+            BIND ( IF ( BOUND(?place_name), ?place_name, "No spatialCoverage") as ?placename ) .
+             BIND ( IF ( BOUND(?kwu), ?kwu, "") as ?kws ) .
+             }
+
+        }
+      #  GROUP BY ?g ?subj ?resourceType ?name ?description ?pubname ?placenames ?kw ?datep    ?sosType 
+       GROUP BY ?g ?subj  ?name ?description ?pubname ?placenames ?kw ?datep    ?sosType 
         """
 test_types="""
 prefix schema: <https://schema.org/>
-SELECT  ?type  (count(distinct ?s ) as ?scount)
+SELECT distinct ?place_name ?g ?type ?sosType ?date_p (GROUP_CONCAT(DISTINCT ?resourceType ; SEPARATOR=", ") AS ?sc) (count(distinct ?s ) as ?scount)
 WHERE {
-{
+graph ?g  {
 
        ?s a ?type .
+             values ?sosType {
+            schema:Dataset
 
+            } 
+             Minus {?s a schema:Person } .
+                  BIND (IF (exists {?s a schema:Dataset .} ||exists{?s a schema:DataCatalog .} , "data", "tool") AS ?resourceType) 
+         OPTIONAL {?s schema:datePublished ?date_p .}
+         OPTIONAL {?s schema:spatialCoverage/schema:name ?place_name .}
+          OPTIONAL {?s schema:publisher/schema:name|schema:sdPublisher|schema:provider/schema:name ?pub_name .}
        }
+ 
 }
 
-GROUP By ?type
+GROUP By ?type ?resourceType ?sosType ?date_p ?place_name
 ORDER By DESC(?scount)
 """
 SCHEMAORG_http = Namespace("http://schema.org/")
@@ -73,11 +121,13 @@ class ReleaseGraph:
         #might just feed the result rows to pandas
         # all_summary_query returns no rows ;)
        # resource = ec.graph.sparql_query._getSparqlFileFromResources("all_summary_query")
-        resource = ec.graph.sparql_query._getSparqlFileFromResources("all_repo_count_datasets")
-       # result = self.dataset.query(resource)
-        result = self.dataset.query(summary_sparql, initNs={'schema_old': SCHEMAORG_http, 'schema':SCHEMAORG_https })
-      #  result = self.dataset.query(test_types, initNs={'schema_o': SCHEMAORG_http, 'schema':SCHEMAORG_https })
-        df = pandas.DataFrame(result)
+       # resource = ec.graph.sparql_query._getSparqlFileFromResources("all_repo_count_datasets")
+        # result = self.dataset.query(resource)
+
+        #result = self.dataset.query(test_types, initNs={'schema_o': SCHEMAORG_http, 'schema':SCHEMAORG_https })
+        result = self.dataset.query(summary_sparql, initNs={'schema_old': SCHEMAORG_http, 'schema': SCHEMAORG_https})
+        #result = self.dataset.query(summary_sparql)
+        df = pandas.DataFrame(result.bindings)
         return df
 
 # types works, summary does not.
