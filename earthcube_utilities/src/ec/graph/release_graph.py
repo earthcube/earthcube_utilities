0bbaa7cb499d059236ff77dd71af118483cfbc35
David Valentine <dwvalentine@ucsd.edu>
2023-10-16 22:07:15 -0700

    minio option read release (#127)
    * add options for minio
    
    * add options for minio
    there can be nt files in latest release, so add extension to method

--- earthcube_utilities/src/ec/graph/release_graph.py
+++ earthcube_utilities/src/ec/graph/release_graph.py
@@ -112,10 +112,10 @@ class ReleaseGraph:
     #dataset = ConjunctiveGraph()
     filename = ""
 
-    def load_release(self, file_or_url):
-        self.dataset.parse(file_or_url, format='nquads')
-    def read_release(self, s3server, s3bucket, source, date="latest"):
-        s3 = MinioDatastore(s3server, None)
+    def load_release(self, file_or_url, format='nquads' ):
+        self.dataset.parse(file_or_url, format=format)
+    def read_release(self, s3server, s3bucket, source, date="latest", options={}):
+        s3 = MinioDatastore(s3server, options)
         url = s3.getLatestRelaseUrl(s3bucket, source)
         self.filename = url[ url.rfind('/') +1 :]
         self.load_release(url)
