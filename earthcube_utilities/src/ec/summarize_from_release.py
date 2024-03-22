68530577b4202e324ed8975fa545b63e5b2ad5dd
David Valentine <david.valentine@gmail.com>
2024-03-22 10:47:30 -0700

    Updated summarize_from_release.py added run configs and quads for testing

--- earthcube_utilities/src/ec/summarize_from_release.py
+++ earthcube_utilities/src/ec/summarize_from_release.py
@@ -4,6 +4,8 @@ import argparse
 import logging
 import os
 from ec.graph.manageGraph import ManageBlazegraph as mg
+from ec.graph.release_graph import ReleaseGraph
+from ec.graph.sparql_query import _getSparqlFileFromResources
 from ec.summarize.summarize_materializedview import summaryDF2ttl, get_summary4graph,get_summary4repoSubset
 from ec.gleanerio.gleaner import endpointUpdateNamespace,getNabu, reviseNabuConfGraph, runNabu
 from rdflib import Dataset
@@ -24,8 +26,7 @@ def summarizeReleaseOnly():
 
                         )
 
-    parser.add_argument("--repo", dest='repo', help='repo name used in the  urn')
-
+    parser.add_argument("--repo", dest='repo', help='repo name used in the  urn', default="dummy")
     parser.add_argument('--s3base', dest='s3base',
                         help='basurl of',
                         default="https://oss.geocodes-dev.earthcube.org/"
@@ -41,81 +42,29 @@ def summarizeReleaseOnly():
                         )
     parser.add_argument('--nographsummary', action='store_true', dest='nographsummary',
                         help='send triples to file', default=False)
-    parser.add_argument('--summary_namespace', dest='summary_namespace',
-                        help='summary_namespace defaults to {repo_summary}',
-                        )
     args = parser.parse_args()
 
-    repo = args.repo
-    if args.summary_namespace:
-        if isValidURL(args.summary_namespace):
-            msg = 'For summary_namespace, Please enter the namespace only.'
-            print(msg)
-            logging.error(msg)
-            return 1
-        summary = args.summary_namespace
-    else:
-        summary = f"{repo}_summary"
+
     endpoint= args.graphendpoint
     graphendpoint = mg.graphFromEndpoint(endpoint)
+    rg = ReleaseGraph()
 
-    g = Dataset()
-    g.parse(args.url, format='nquads')
-    ## HOW TO SUMMARIZE from RDF QUadss?
-    # this will need to be modularlized, ad reqworked.
 
-    file = '../resources/sparql/all_summary_query.sparql'
-    with open(file, 'r') as f:
-        lines = f.read()
-    sumresults = g.query(lines)
+
+
 ## this returns no rows.
     # WE CAN USE BLAZEGRAPH, so no use going direct
 
 
     try:  # temp has been created
         # created = tempnsgraph.createNamespace()
-        # if ( created=='Failed'):
-        #     logging.fatal("coould not create namespace")
-        sumnsgraph = mg(graphendpoint, summary)
-        # created = sumnsgraph.createNamespace()
-        # if ( created=='Failed'):
-        #     logging.fatal("coould not create summary namespace")
-        # endpoints for file
-        # tempendpoint =endpointUpdateNamespace(endpoint,f"{repo}_temp")
-
-        summaryendpoint =endpointUpdateNamespace(endpoint,summary)
-
-        # newNabucfg = reviseNabuConf(cfg,tempendpoint )
-        # runNabu(newNabucfg,repo, args.glcon )
-
-        if repo is not None:
-            summarydf = get_summary4repoSubset(endpoint, repo)
-        else:
-            # this really needs to be paged ;)
-            summarydf = get_summary4graph(endpoint)
-            repo = ""
-
-        nt,g = summaryDF2ttl(summarydf,repo) # let's try the new generator
-
+        rg.load_release(args.url)
+        summarydf = rg.summarize()
+        nt, g = summaryDF2ttl(summarydf, args.repo)  # let's try the new generator
         summaryttl = g.serialize(format='longturtle')
-        # write to s3  in future
-        # with open(os.path.join("output",f"{repo}.ttl"), 'w') as f:
-        #      f.write(summaryttl)
-        if not args.nographsummary:
-            inserted = sumnsgraph.insert(bytes(summaryttl, 'utf-8'),content_type="application/x-turtle" )
-            if inserted:
-                logging.info(f"Inserted into graph store{sumnsgraph.namespace}" )
-            else:
-                logging.error(f" dumping file {repo}.ttl  Repo {repo} not inserted into {sumnsgraph.namespace}")
 
-                with open(os.path.join("output",f"{repo}.ttl"), 'w') as f:
-                     f.write(summaryttl)
-                return 1
-        else:
-            logging.info(f" dumping file {repo}.ttl  nographsummary: {args.nographsummary} ")
-
-            with open(os.path.join("output", f"{repo}.ttl"), 'w') as f:
-                f.write(summaryttl)
+        with open(os.path.join("output", f"{args.repo}.ttl"), 'w') as f:
+            f.write(summaryttl)
     except Exception as ex:
         logging.error(f"error {ex}")
         print(f"Error: {ex}")
