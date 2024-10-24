e4d065591ed36631c0b812fa7a9c3461003379d6
Dipannita Dey <ddey2@illinois.edu>
2024-10-24 10:26:06 -0500

    trimmed date to year

--- earthcube_utilities/src/ec/summarize/summarize_materializedview.py
+++ earthcube_utilities/src/ec/summarize/summarize_materializedview.py
@@ -128,6 +128,9 @@ def summaryDF2ttl(df: pandas.DataFrame, repo: str, from_release=False) -> tuple[
         datep=row['datep']
         if datep == "No datePublished":
             datep=pandas.NA
+        else:
+            # Truncate to year
+            datep = datep.split('-')[0]
         # Query should not return "No datePublished" is not a valid Date "YYYY-MM-DD" so
         # UI Date Select failed, because it expects an actual date
         #   Empty values might be handled in the UI...,
